eb4da7643a179038c33da7d99d541c9e9894dae2
gairal <frank@gairal.com>
2024-05-08 02:06:34 +0200

    fix: quotes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,12 +1,12 @@
 const {
-  APP_NAME = 'koa-seed',
-  LOG_LEVEL = 'info',
-  NODE_ENV = 'development',
+  APP_NAME = "koa-seed",
+  LOG_LEVEL = "info",
+  NODE_ENV = "development",
 } = process.env;
 
-const IS_DEV = NODE_ENV === 'development';
-const IS_PROD = NODE_ENV === 'production';
-const IS_TEST = NODE_ENV === 'test';
+const IS_DEV = NODE_ENV === "development";
+const IS_PROD = NODE_ENV === "production";
+const IS_TEST = NODE_ENV === "test";
 
 export const config = {
   appName: APP_NAME,
--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,6 +1,6 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, transports } from "winston";
 
-import { config } from './config';
+import { config } from "./config";
 
 export const logger = createLogger({
   defaultMeta: { appName: config.appName },
