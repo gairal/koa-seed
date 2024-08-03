f38558f53fb913116caa263bb6436ba4eb6ecbad
gairal <frank@gairal.com>
2024-08-03 09:56:19 +0200

    feat: biome

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,6 @@
 export default {
   // Using fake extensions is a trick to run tasks concurrently.
   // See https://github.com/okonet/lint-staged/issues/934
-  // Run type-check to all TypeScript files
   "*.{ts,type-check}": () => "bun typecheck",
-  // Run ESLint on changed TypeScript files
-  "*.{ts,lint}": (filenames) => `bun lint --fix ${filenames.join(" ")}`,
-  "*.{json,md,prettierrc,eslintrc}": (filenames) =>
-    `prettier --write ${filenames.join(" ")}`,
+  "*.{ts,json,lint}": (filenames) => `bun lint --fix ${filenames.join(" ")}`,
 };
